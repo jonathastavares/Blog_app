0e72affbed8d1702e64ebd4f81c6d298c27fafd3
jonathastavares <jonathastavares@id.uff.br>
2021-03-22 15:08:37 -0300

    Create new article setup, requirements working, error messages working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,13 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        #render plain: params {:article}.inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_path(@article)
+        if @article.save
+            flash[:notice] = "Article was successfully created!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 
     private
