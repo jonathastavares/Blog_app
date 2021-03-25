d07e43cb3b1d32ff82d951e7a3f02c3950d547d9
jonathastavares <jonathastavares@id.uff.br>
2021-03-25 15:44:43 -0300

    Fixed routing problems, Created Index to list all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,34 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created!"
-            redirect_to article_path(@article)
+            flash[:notice] = "Article was successfully created"
+            redirect_to show_path(@article)
         else
             render 'new'
         end
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated"
+            redirect_to show_path(@article)
+        else
+            flash[:notice] = "Article was not updated"
+            render 'edit'
+        end
+    end
+
+    def index
+        @articles = Article.all
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'about', to: 'pages#about'
-  get 'article', to: 'articles#new'
+  get '/articles', to: 'articles#new', as: 'article'
+  get '/articles/:id(.:format)', to: 'articles#show', as: 'show'
+  get '/index', to: 'articles#index', as: 'index'
   resources :articles
 end
