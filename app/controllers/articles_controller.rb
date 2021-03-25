dde34ac79c7a35518248c82c8cdcab498f76fdc8
jonathastavares <jonathastavares@id.uff.br>
2021-03-25 17:24:44 -0300

    Created Edit, Show and Delete functionalities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
-            redirect_to show_path(@article)
+            redirect_to article_path(@article)
         else
             render 'new'
         end
@@ -25,15 +25,22 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was updated"
-            redirect_to show_path(@article)
+            redirect_to articles_path(@article)
         else
             flash[:notice] = "Article was not updated"
             render 'edit'
         end
-    end
+      end
 
     def index
         @articles = Article.all
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path
     end
 
     private
