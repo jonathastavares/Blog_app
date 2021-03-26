53154fde31323487e934572ae5bda327047dd100
jonathastavares <jonathastavares@id.uff.br>
2021-03-26 20:58:10 -0300

    Created sessions_controller to login/logout users

--- Gemfile
+++ Gemfile
@@ -40,6 +40,10 @@ gem 'therubyracer'
 
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'will_paginate', '3.3.0'
+
+gem 'bootstrap-will_paginate', '~> 1.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
   resources :articles
 end
