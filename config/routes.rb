a0919f66abde60caf42c678766d80081a64d1606
jonathastavares <jonathastavares@id.uff.br>
2021-04-01 00:50:35 -0300

    Styled the blog with blue theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'articles#index'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
