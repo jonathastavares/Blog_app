ca3753c5aea66b12815162a18470fc1a7c46bee4
jonathastavares <jonathastavares@id.uff.br>
2021-04-02 12:09:29 -0300

    Fixed Rubocop errors #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    !!current_user
+    !current_user.nil?
   end
 
   def require_user
-    unless logged_in?
-      flash[:danger] = 'You must be logged in to perform that action'
-      redirect_to root_path
-    end
+    return if logged_in?
+
+    flash[:danger] = 'You must be logged in to perform that action'
+    redirect_to root_path
   end
 end
